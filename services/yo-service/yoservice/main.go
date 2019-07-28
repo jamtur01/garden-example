e0c0a6ae92399cd2e68b31f448678d5dd9e929dd
James Turnbull <james@lovedthanlost.net>
2019-07-28 17:50:11 -0400

    Fixed formatting

--- services/yo-service/yoservice/main.go
+++ services/yo-service/yoservice/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"fmt"
-        "io/ioutil"
-        "log"
-	"net/http"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "net/http"
 )
 
 func yo(w http.ResponseWriter, r *http.Request) {
-        res, err := http.Get("http://mate-service/mate")
-	if err != nil {
-		log.Fatal(err)
-	}
-	mate, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprint(w, "Yo", " ", string(mate))
+
+  res, err := http.Get("http://mate-service/mate")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  mate, err := ioutil.ReadAll(res.Body)
+  res.Body.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Fprint(w, "Yo", " ", string(mate))
 }
 
 func main() {
-	http.HandleFunc("/yo", yo)
+  http.HandleFunc("/yo", yo)
 
-	fmt.Println("Server running...")
-        log.Fatal(http.ListenAndServe(":8080", nil))
+  fmt.Println("Server running...")
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
